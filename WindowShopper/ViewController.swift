9d22c1a1477c0e86eaa1658699a14c05eee3365a
David Murillo <davidmurillo@davids-imac.home>
2020-05-06 17:39:10 -0700

    First Commit

---
+++ WindowShopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WindowShopper
+//
+//  Created by David Murillo on 5/4/20.
+//  Copyright © 2020 MuriTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
